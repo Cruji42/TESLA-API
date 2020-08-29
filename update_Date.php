cfdfdd9569a201ae3e7a0cf4fbb83881e5c1dd13
Cruji42 <l.move971223@itses.edu.mx>
2020-08-28 22:19:08 -0500

    Update

---
+++ update_Date.php
@@ -0,0 +1,27 @@
+<?php
+require "vendor/autoload.php";
+include_once "utils/headers.php";
+use DBC\Conexion as dbc;
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+@$Option = $request-> option;
+@$Id = $request-> id;
+
+if($Option != '' && $Id != ''){
+    if($Option == 'A'){
+        $query = "UPDATE TABLE test set state = 'Aceptada' where id= '$Id'";
+        $result = dbc::Query($query);
+        echo json_encode($result);
+    }else if ($Option == 'B'){
+        $query = "UPDATE TABLE test set state = 'Rechazada' where id= '$Id'";
+        $result = dbc::Query($query);
+        echo json_encode($result);
+
+    }else{
+        echo json_encode('No se pudo actualizar');
+    }
+}else{
+    echo json_encode('Error');
+}
