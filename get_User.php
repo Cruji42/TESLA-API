044b702130dfd924c3b32e450091183572821f52
Cruji42 <l.move971223@itses.edu.mx>
2020-08-28 21:59:58 -0500

    All dates

--- get_User.php
+++ get_User.php
@@ -9,16 +9,7 @@ $request = json_decode($postdata);
 @$User = $request-> id;
 
 if($User != ''){
-    $query = " SELECT t1.id, t4.nickname  as Client, t1.date, t2.name as Vehicle, t2.image, t3.name, t1.state FROM
-test as t1
-JOIN products as t2
-ON t1.products_id = t2.id
-JOIN stores as t3
-ON t1.stores_id = t3.id
-JOIN users as t4 
-ON t1.users_id = t4.id
-WHERE t4.id = '$User'";
-
+    $query = " SELECT * FROM users WHERE id = '$User'";
     $result = dbc::Query($query);
     echo json_encode($result);
 }else{
