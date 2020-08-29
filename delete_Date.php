38671e848e66b2f4f1c3cb95f875ac1aae64116f
Cruji42 <l.move971223@itses.edu.mx>
2020-08-29 03:46:51 -0500

    Finish

--- delete_Date.php
+++ delete_Date.php
@@ -2,16 +2,23 @@
 require "vendor/autoload.php";
 include_once "utils/headers.php";
 use DBC\Conexion as dbc;
+use AUTH\Auth as token;
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 @$Id = $request-> id_cite;
+@$Token = $request-> token;
 
-if($Id != ''){
-    $query = " DELETE FROM test WHERE id = '$Id'";
-    $result = dbc::Insert($query);
-    echo json_encode($result);
+if(token::tokenvalidate($Token)){
+    if($Id != ''){
+        $query = " DELETE FROM test WHERE id = '$Id'";
+        $result = dbc::Insert($query);
+        echo json_encode($result);
+    }else{
+        echo json_encode('Error');
+    }
 }else{
-    echo json_encode('Error');
+    echo json_encode('TokenEXP');
 }
+
--- update_Date.php
+++ update_Date.php
@@ -2,26 +2,33 @@
 require "vendor/autoload.php";
 include_once "utils/headers.php";
 use DBC\Conexion as dbc;
+use AUTH\Auth as token;
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
+@$Token = $request-> token;
 @$Option = $request-> option;
 @$Id = $request-> id;
 
-if($Option != '' && $Id != ''){
-    if($Option == 'A'){
-        $query = "UPDATE test set state = 'Aceptada' where id= '$Id'";
-        $result = dbc::Insert($query);
-        echo json_encode($result);
-    }else if ($Option == 'B'){
-        $query = "UPDATE test set state = 'Rechazada' where id= '$Id'";
-        $result = dbc::Insert($query);
-        echo json_encode($result);
 
+if(token::tokenvalidate($Token)){
+    if($Option != '' && $Id != ''){
+        if($Option == 'A'){
+            $query = "UPDATE test set state = 'Aceptada' where id= '$Id'";
+            $result = dbc::Insert($query);
+            echo json_encode($result);
+        }else if ($Option == 'B'){
+            $query = "UPDATE test set state = 'Rechazada' where id= '$Id'";
+            $result = dbc::Insert($query);
+            echo json_encode($result);
+
+        }else{
+            echo json_encode('No se pudo actualizar');
+        }
     }else{
-        echo json_encode('No se pudo actualizar');
+        echo json_encode('Error');
     }
 }else{
-    echo json_encode('Error');
+    echo json_encode('TokenEXP');
 }
